d7fee604190728c58935f3b6f24da2165f01b1e9
Joseph Mate <mate.joseph@gmail.com>
2020-04-21 21:34:05 -0400

    partially working. alert message box does not work

---
+++ rust_and_web_assembly/hello-world/run.sh
@@ -0,0 +1,7 @@
+
+wasm-pack build --target web
+
+# python 3
+python -m http.server
+
+
--- rust_and_web_assembly/hello-world/src/lib.rs
+++ rust_and_web_assembly/hello-world/src/lib.rs
@@ -15,5 +15,24 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+    alert("Hello world!");
+}
+
+// Called when the wasm module is instantiated
+// Example appends <p>Hello from Rust!</p> to the body.
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+	
+    body.append_child(&val)?;
+
+    Ok(())
 }
