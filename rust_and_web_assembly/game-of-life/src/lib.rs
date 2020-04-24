7845002de5b5d3154498fbc7c79dc1689783b6fd
Joseph Mate <mate.joseph@gmail.com>
2020-04-23 20:49:22 -0400

    copied hello world project to game of life

---
+++ rust_and_web_assembly/game-of-life/src/lib.rs
@@ -0,0 +1,38 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello world!");
+}
+
+// Called when the wasm module is instantiated
+// Example appends <p>Hello from Rust!</p> to the body.
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+	
+    body.append_child(&val)?;
+
+    Ok(())
+}
